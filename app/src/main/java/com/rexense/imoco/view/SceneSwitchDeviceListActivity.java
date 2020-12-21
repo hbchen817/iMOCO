3a957bf3ba5425da9aaa7fdedd1e33d9edba5296
1002199087 <1002199087@qq.com>
2020-12-21 20:07:07 +0800

    首页添加空调管制面板、拉菲三键盘开关状态信息

--- app/src/main/java/com/rexense/imoco/view/SceneSwitchDeviceListActivity.java
+++ app/src/main/java/com/rexense/imoco/view/SceneSwitchDeviceListActivity.java
@@ -2,6 +2,8 @@ package com.rexense.imoco.view;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
@@ -77,6 +79,17 @@ public class SceneSwitchDeviceListActivity extends BaseActivity {
         initView();
 //        startGetDeviceList();
         getDeviceData();
+
+        initStatusBar();
+    }
+
+    // 嵌入式状态栏
+    private void initStatusBar() {
+        if (Build.VERSION.SDK_INT >= 23) {
+            View view = getWindow().getDecorView();
+            view.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
+            getWindow().setStatusBarColor(Color.WHITE);
+        }
     }
 
     @Override
