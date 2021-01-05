b7be35c24d1185bbda6fb921a8a2c22939a1c844
1002199087 <1002199087@qq.com>
2021-01-05 19:02:53 +0800

    集成bugly

--- app/src/main/java/com/laffey/smart/presenter/MocoApplication.java
+++ app/src/main/java/com/laffey/smart/presenter/MocoApplication.java
@@ -46,7 +46,7 @@ public class MocoApplication extends AApplication {
 
         //设置日志级别
         Logger.setLogLevel(2);
-        CrashReport.initCrashReport(getApplicationContext(), "9b346e3393", BuildConfig.DEBUG);
+        CrashReport.initCrashReport(getApplicationContext(), "e66b11bc4e", BuildConfig.DEBUG);
 
         //安装MultiDex
         MultiDex.install(this);
